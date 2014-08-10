feed16ad0ac44c642271c572a9ba39c9308a9515
saurabh <saurabhkamath@gmail.com>
2014-08-11 02:57:30 +0530

    Forgot to add figures to repo

---
+++ plot1.R
@@ -0,0 +1,8 @@
+Class <- c(rep("character",2),rep("numeric",7))
+Sample<- read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings = "?",colClasses=Class)
+Data <- subset(Sample,Date == "1/2/2007" |  Date == "2/2/2007" )
+Data$Date = as.Date(Data$Date,format="%d/%m/%Y")
+Data$Time = strptime(Data$Time,format="%H:%M:%S")
+hist(Data$Global_active_power,main="Global Active Power",col="red",xlab="Global Active Power (kilowatts)")
+dev.copy(png,"plot1.png",width = 480, height = 480)
+dev.off()
